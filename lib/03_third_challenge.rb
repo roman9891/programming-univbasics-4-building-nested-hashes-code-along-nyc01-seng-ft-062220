7e33edc42aae8d03d1615593d0e5d0a499c1d3bc
Roman Opalacz <roman9891@gmail.com>
2020-05-28 16:34:05 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -22,7 +22,7 @@ def third_challenge
       :patriarch => {
         #your key/value pairs here
           name: "Lord Capulet",
-          age: "51"
+          age: "50"
         },
       :matriarch => {
         #your key/value pairs here
